99fd18141678b3ecff4df64bd64bd99ae1d5004b
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-09-04 17:23:07 -0400

    Added AI Gateway, building out API for frontend

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 
 import { Hono } from "hono"
-import ui from "ui.html";
-import write from "write.html";
-import { generateVectorEmbedding, getAiImageDescription } from "./utils";
+import ui from "./ui.html";
+import write from "./write.html";
+import { generateVectorEmbedding, getAiImageDescription, getAiImageDescriptionNew, getWelcomeMessage } from "./utils";
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use('*',   cors({
+    origin: 'http://localhost:3000',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'],
+    allowMethods: ['POST', 'GET'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  }))
 
 app.get("/", (c: any) => {
 	return c.html(ui);
@@ -19,6 +29,12 @@ app.get("/vectors", async (c: any) => {
 
 app.get('/write', async (c) => {
 	return c.html(write);
+})
+
+app.get("/welcome", async (c: any) => {
+
+	const message =  await getWelcomeMessage(c);
+	return message	;
 })
 
 app.get("/guess", async (c: any) => {
@@ -49,6 +65,48 @@ app.get("/guess", async (c: any) => {
 	const similarityScore = vectorQuery.matches[0].score;
 
 	return c.text(`The AI generated description is: ${aiGeneratedDescription} \n\n The similarity score between the AI generated description and your guess is: ${similarityScore}`);
+})
+
+app.post("/getAiImageDescription", async (c: any) => {
+	const body = await c.req.json()
+	console.log("body", body)
+	const encodedImage = body.encodedImage;
+	const aiGeneratedDescription = await getAiImageDescriptionNew(c, encodedImage)
+
+	return c.json({aiImageDescription: aiGeneratedDescription});
+})
+
+app.post("/guess", async (c: any) => {
+	const body = await c.req.json()
+	console.log("body", body)
+	const guess = body.text;
+	console.log(guess)
+	// const guess = c.req.query("text") || "A field of tall grass with bunnies dancing around";
+
+	const aiGeneratedDescription = await getAiImageDescription(c)
+	console.log('Location 1')
+	const aiVectorValues = await generateVectorEmbedding(c, aiGeneratedDescription);
+	console.log('Location 2')
+
+	const inserted = await c.env.VECTORIZE.upsert([
+		{
+			id: "1",
+			values: aiVectorValues
+		},
+	]);
+	console.log('Location 3')
+
+
+	const userVectorValues = await generateVectorEmbedding(c, guess);
+	console.log('Location 4')
+
+	const vectorQuery = await c.env.VECTORIZE.query(userVectorValues, { topK: 1 });
+	console.log('Location 5')
+
+	console.log(vectorQuery);
+	const similarityScore = vectorQuery.matches[0].score;
+
+	return c.json({message: `The AI generated description is: ${aiGeneratedDescription} \n\n The similarity score between the AI generated description and your guess is: ${similarityScore}`});
 })
 
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,35 +1,85 @@
 async function getAiImageDescription(c: any): Promise<string> {
 	try {
-		const res = await fetch("https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/1761b7e9-42ba-4081-ab48-06796d20b500/public");
+		const res = await fetch('https://imagedelivery.net/llMDWXFPgX44M9elMfQ9XA/1761b7e9-42ba-4081-ab48-06796d20b500/public');
 		const blob = await res.arrayBuffer();
 		const input = {
 			image: [...new Uint8Array(blob)],
-			prompt: "Describe the image in a concise sentence",
+			prompt: 'Describe the image in a concise sentence',
 			max_tokens: 512,
 		};
-		const response = await c.env.AI.run(
-			"@cf/llava-hf/llava-1.5-7b-hf",
-			input
-		);
+		const response = await c.env.AI.run('@cf/llava-hf/llava-1.5-7b-hf', input, {
+			gateway: {
+				id: 'are-you-ai-gateway',
+				skipCache: false,
+				cacheTtl: 3360,
+			},
+		});
 
 		return response.description;
 	} catch (error) {
-		return "Error";
+		return 'Error';
+	}
+}
+
+async function getAiImageDescriptionNew(c: any, encodedImage: any): Promise<string> {
+	try {
+		const input = {
+			image: encodedImage,
+			prompt: 'Describe the image in a concise sentence',
+			max_tokens: 512,
+		};
+		const response = await c.env.AI.run('@cf/llava-hf/llava-1.5-7b-hf', input, {
+			gateway: {
+				id: 'are-you-ai-gateway',
+				skipCache: false,
+				cacheTtl: 3360,
+			},
+		});
+
+		return response.description;
+	} catch (error) {
+		return 'Error';
 	}
 }
 
 const generateVectorEmbedding = async (c: any, text: string) => {
-	const { data } = await c.env.AI.run("@cf/baai/bge-base-en-v1.5", {
-		text: [text],
-	});
+	const { data } = await c.env.AI.run(
+		'@cf/baai/bge-base-en-v1.5',
+		{
+			text: [text],
+		},
+		{
+			gateway: {
+				id: 'are-you-ai-gateway',
+				skipCache: false,
+				cacheTtl: 3360,
+			},
+		}
+	);
 	const values = data[0];
 
 	if (!values) {
-		return c.text("Failed to generate vector embedding", 500);
+		return c.text('Failed to generate vector embedding', 500);
 	}
 
 	return values;
 };
 
+async function getWelcomeMessage(c: any): Promise<Response> {
+	try {
+		const messages = [
+			{ role: "system", content: "Thomas is joining you and will be competing against AI to see how similarly they can describe an image to another AI model. You should simply welcome them to the challenge, i already have the images generated. Ask for a consise description of the image shown on the screen." },
+			{
+			  role: "user",
+			  content: "Hi, I'm Thomas. I'm excited to see how well I can describe an image compared to an AI model. Let's get started!",
+			},
+		  ];
+		  const response = await c.env.AI.run("@hf/thebloke/llama-2-13b-chat-awq", { messages });
 
-export { getAiImageDescription,  generateVectorEmbedding }
+		  return Response.json(response);
+	} catch (error) {
+		return new Response('Failed to get welcome message');
+	}
+}
+
+export { getAiImageDescription, getAiImageDescriptionNew, generateVectorEmbedding, getWelcomeMessage };

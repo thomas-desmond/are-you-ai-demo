abd04b09b0629ab1ec3bd06e15e8883f569363ff
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-09-19 14:52:16 -0700

    Type error fix

--- lib/ai.ts
+++ lib/ai.ts
@@ -45,7 +45,7 @@ async function getRandomAIGeneratedImage(): Promise<string> {
     },
   });
   
-  const data = await response.json();
+  const data = await response.json() as any;
   console.log("response", data);
 
   // const data = (await response.json()) as any;

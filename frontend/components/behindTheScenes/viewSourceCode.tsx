d3d1b732138959e808dd5a58ae71f8d2f3941b3d
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-09-26 16:08:45 -0700

    New view source code UI

--- frontend/components/behindTheScenes/viewSourceCode.tsx
+++ frontend/components/behindTheScenes/viewSourceCode.tsx
@@ -1,33 +1,38 @@
 import { Button } from "@/components/ui/button";
 import { Github } from "lucide-react";
 import Link from "next/link";
+import { Card, CardContent, CardFooter } from "../ui/card";
 
 export default function ViewSourceCode({}) {
   return (
-    <div className="bg-white rounded-lg mb-4 p-6 shadow-lg">
-      <h2 className="text-2xl font-bold mb-4">View the Source Code</h2>
-      <div className="space-y-4">
-        <Link
-          href="https://github.com/thomas-desmond/are-you-ai-frontend"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button className="w-full mb-2">
-            <Github className="mr-2 h-4 w-4" />
-            Frontend Repository
-          </Button>
-        </Link>
-        <Link
-          href="https://github.com/thomas-desmond/are-you-ai-api"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button className="w-full mb-2">
-            <Github className="mr-2 h-4 w-4" />
-            API Repository
-          </Button>
-        </Link>
-      </div>
-    </div>
+    <Card className="w-full mx-auto bg-gradient-to-r from-[#FBAD41] via-[#F6821F] to-[#F6821F] text-black mb-4">
+      <CardContent className="flex items-center justify-between p-6">
+        <div className="space-y-1">
+          <h3 className="text-2xl font-bold">Source Code</h3>
+          <p className="">
+            Explore the GitHub repository powering this application
+          </p>
+        </div>
+        <CardFooter className="p-0">
+          <Link
+            href="https://github.com/thomas-desmond/are-you-ai-demo"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button
+              variant="outline"
+              size="lg"
+              className="text-primary hover:text-primary-foreground"
+              asChild
+            >
+              <div>
+                <Github className="mr-2 h-5 w-5" />
+                View on GitHub
+              </div>
+            </Button>
+          </Link>
+        </CardFooter>
+      </CardContent>
+    </Card>
   );
 }

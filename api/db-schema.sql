c7baba040464328e82e154672bfd6d8e328feedf
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-09-26 16:08:35 -0700

    Create Table schema

---
+++ api/db-schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS Sessions (ID INTEGER PRIMARY KEY, sessionID TEXT, username TEXT, email TEXT, date DATETIME, score INTEGER, aiImageDescription TEXT, userDescription TEXT, imageUrl TEXT);
+

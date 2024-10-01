2fa71f33db8198cfae4a8e55855ff4aff3de2e3b
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-10-01 09:11:15 -0700

    Update privacy and terms of use

--- frontend/components/ui/footer.tsx
+++ frontend/components/ui/footer.tsx
@@ -4,12 +4,18 @@ import BehindTheScenesButton from "../behindTheScenes/behindTheScenesButton";
 export default function Footer() {
   return (
     <div className="bg-gray-300 text-black py-2 w-full px-5 sticky bottom-0">
-          <nav className="flex flex-row justify-between items-center">
-            <Link href="https://www.cloudflare.com/website-terms/" className="">
-              Privacy Policy
-            </Link>
-            <BehindTheScenesButton />
-          </nav>
+      <nav className="flex flex-row justify-between items-center">
+        <div>
+          <Link href="https://www.cloudflare.com/website-terms/" className="">
+            Terms of Use
+          </Link>
+          {" | "}
+          <Link href="https://www.cloudflare.com/privacypolicy/" className="">
+            Privacy Policy
+          </Link>
+        </div>
+        <BehindTheScenesButton />
+      </nav>
     </div>
   );
 }

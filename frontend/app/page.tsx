416f10fe1c77cfaca5404450a39b1e7aa39b83c7
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-10-04 13:26:28 -0700

    Remove ContentWrapper from front page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,14 +7,7 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-full items-center justify-center">
-      <ComponentWrapper
-        imageUrls={["/services/pages.svg"]}
-        imageToolTips={[toolTipData.pages,]}
-        imagePosition="bottom-right"
-      >
-        {" "}
         <AppInfoCard />
-      </ComponentWrapper>
     </div>
   );
 }

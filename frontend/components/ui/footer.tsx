40494e2f1cccfd4b3129ce96410e81d850987a1c
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-10-08 14:47:40 -0700

    Reducing image and text size

--- frontend/components/ui/footer.tsx
+++ frontend/components/ui/footer.tsx
@@ -3,14 +3,14 @@ import BehindTheScenesButton from "../behindTheScenes/behindTheScenesButton";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-300 text-black py-2 w-full px-5 sticky bottom-0">
+    <div className="bg-gray-300 text-black py-1 w-full px-5 sticky bottom-0">
       <nav className="flex flex-row justify-between items-center">
         <div>
-          <Link href="https://www.cloudflare.com/website-terms/" className="">
+          <Link href="https://www.cloudflare.com/website-terms/" className="text-sm">
             Terms of Use
           </Link>
           {" | "}
-          <Link href="https://www.cloudflare.com/privacypolicy/" className="">
+          <Link href="https://www.cloudflare.com/privacypolicy/" className="text-sm">
             Privacy Policy
           </Link>
         </div>

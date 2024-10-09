24b2e25bea9e59d3a9e9044e8a4a836f18b54659
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-10-09 09:36:19 -0700

    Bring database insert into server action

--- frontend/app/actions/UserDescriptionSubmitAction.tsx
+++ frontend/app/actions/UserDescriptionSubmitAction.tsx
@@ -1,29 +1,30 @@
 "use server";
 
 import { getAiSimilarity } from "@/lib/ai";
+import { insertIntoDatabase } from "@/lib/db";
 
 const handleSubmitServerAction = async (prevState: any, formData: FormData) => {
-      const text = formData.get("message") as string;
-    if (!text) return;
-  
-    const sessionId = formData.get("sessionId") as string;
-    // const aiImageDescription = formData.get("aiImageDescription") as string;
-    // const imageUrl = formData.get("imageUrl") as string;
-  
-    const score = await getAiSimilarity(sessionId, text);
+  const text = formData.get("message") as string;
+  if (!text) return;
 
-    // await insertIntoDatabase(
-    //   sessionId,
-    //   text,
-    //   aiImageDescription,
-    //   parseFloat((score * 100).toFixed(3)),
-    //   imageUrl
-    // );
-  
-    return {
-      similarityScore: parseFloat((score * 100).toFixed(3)),
-      userDescription: text,
-    };
+  const sessionId = formData.get("sessionId") as string;
+  const aiImageDescription = formData.get("aiImageDescription") as string;
+  const imageUrl = formData.get("imageUrl") as string;
+
+  const score = await getAiSimilarity(sessionId, text);
+
+  await insertIntoDatabase(
+    sessionId,
+    text,
+    aiImageDescription,
+    parseFloat((score * 100).toFixed(3)),
+    imageUrl
+  );
+
+  return {
+    similarityScore: parseFloat((score * 100).toFixed(3)),
+    userDescription: text,
   };
+};
 
-  export { handleSubmitServerAction }
+export { handleSubmitServerAction };
